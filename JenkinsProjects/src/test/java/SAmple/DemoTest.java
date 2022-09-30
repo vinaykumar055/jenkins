25cff81fc0ce912b2d212b938b36696495ec57f3
hp <hp@DESKTOP-D9BKGLB>
2022-09-30 15:05:06 +0530

    commit

---
+++ JenkinsProjects/src/test/java/SAmple/DemoTest.java
@@ -0,0 +1,16 @@
+package SAmple;
+
+import org.testng.annotations.Test;
+
+public class DemoTest 
+{
+	@Test
+public void regression()
+{
+	System.out.println("regression test case passed");
+}
+	@Test
+public void systemttest() {
+	System.out.println("systemtest passed");
+}
+}
